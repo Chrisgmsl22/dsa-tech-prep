9cbd27290ce6a2a9eb7d32aa5316f2f269be7f1e
Christian Gonzalez <chrisgmag22@gmail.com>
2026-05-19 19:01:48 -0600

    heaps, top K freq elements

---
+++ patterns/heaps/topKFrequentElements.py
@@ -0,0 +1,70 @@
+""""
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+
+Output: [1]
+
+Example 3:
+
+Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+Output: [1,2]
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+
+"""
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        freqCounter: dict[int, int] = {}
+        minHeap = []
+
+        for num in nums:
+            freqCounter[num] = freqCounter.get(num, 0) + 1
+        
+        for key, val in freqCounter.items():
+            heapq.heappush(minHeap, (val, key)) # Val and Key (order is reversed)
+            print("current state of minHeap: ", minHeap)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+                print("state after pop: ", minHeap)
+        
+        #print("minHeap: ", minHeap)
+        
+        ans = [t[1] for t in minHeap]
+        return ans
+
+
+
+sol = Solution()
+
+nums = [1,1,1,2,2,3] 
+k = 2
+
+res = sol.topKFrequent(nums, k)
+
+print("final res: ", res)
